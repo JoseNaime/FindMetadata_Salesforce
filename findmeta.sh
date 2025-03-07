b8ebe02ee7d46edf4311c6e567b297b1a89c153d
Jose Pablo Naime <jnaime@jnaime-ltmp3v8.internal.salesforce.com>
2025-03-06 18:38:36 -0600

    d flag added

--- findmeta.sh
+++ findmeta.sh
@@ -9,7 +9,7 @@ EXPERIENCE_BG="\e[42m"   # Green background
 DEFAULT_BG="\e[47m"      # White background for 'others'
 RESET="\e[0m"
 
-# Helper function: returns a background color based on folder name
+# Helper functions
 getCategoryColor() {
   case "$1" in
     objects|layouts|fields|recordTypes|pages)
@@ -33,37 +33,79 @@ getCategoryColor() {
   esac
 }
 
+usage() {
+  cat << 'EOF'
+Usage: findmeta [options] <directory> <search_term>
+
+Options:
+  -h       Display this help message and exit.
+  -d       Enable details on findings.
+
+Example:
+  ./findmeta.sh -d /path/to/directory search_term
+EOF
+}
+
+############################################################
+# Capture flags
+############################################################
+flag_details=false
+
+while getopts "h|d" opt; do
+  case "$opt" in
+    h)
+      usage
+      exit 0
+      ;;
+    d)
+      echo "Detected flag -d"
+      flag_details=true
+      ;;
+    *)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Shift away the processed options
+shift $((OPTIND - 1))
+
 ############################################################
 # Search word logic
 ############################################################
 
-# Validate arguments
 if [ $# -lt 2 ]; then
-  echo "Usage: findmeta [directory] [search_term]"
+  echo "Usage: findmeta [options] <directory> <search_term>"
   exit 1
 fi
 
 SEARCH_DIR="$1"
 SEARCH_TERM="$2"
 
-# Keep track of the last folder name displayed to group results
 last_folder=""
 
-# Use find to list all files in the given directory, then loop through them
 find "$SEARCH_DIR" -type f 2>/dev/null | sort | while read -r file; do
   # Check if file contains the search term (case-insensitive)
-  if grep -qi "$SEARCH_TERM" "$file"; then
-
+ if grep -qi "$SEARCH_TERM" "$file"; then
     folder=$(basename "$(dirname "$file")")
     filename=$(basename "$file")
 
-    # Print folder name only if it's different from the last printed one
     if [ "$folder" != "$last_folder" ]; then
         colorBG=$(getCategoryColor "$folder")
         printf "${colorBG}>> ${folder}${RESET}\n"
         last_folder="$folder"
     fi
 
-    printf "\t $filename \"$SEARCH_TERM\" \n"
+    printf "\t $filename \n"
+
+    # Print matching lines with line numbers
+    if [ "$flag_details" = true ]; then
+      grep -in --color=always "$SEARCH_TERM" "$file" | while read -r line; do
+        printf "\t\t%s\n" "$line"
+      done
+
+    fi
   fi
 done
+
